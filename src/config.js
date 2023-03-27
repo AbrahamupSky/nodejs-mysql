c9ec10d3ad81ea11c71c4053a1aa15d9da1a4398
Abrahamup <abrahamup7@gmail.com>
2023-03-27 00:37:18 -0600

    first commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 3000;
+
+export const DB_HOST = process.env.DB_HOST || "locarhost";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORDD || "GatitosBonitos";
+export const DB_NAME = process.env.DB_NAME || "userDB";
+export const DB_PORT = process.env.DB_PORT || 3306;
